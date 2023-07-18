e74b19e3c185d83226e991e93e27d04a1f3e4f46
Wilker Reyes <44204265+wreyes93@users.noreply.github.com>
2023-07-18 17:59:35 -0300

    Force a e2e to fail

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,9 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('fails', () => {
+    cy.visit('https://example.cypress.io')
+    expect(true).to.be.false; // This assertion will fail
+  })
 })
